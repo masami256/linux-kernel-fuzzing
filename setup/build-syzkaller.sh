b4ae6edbd554a8e2d47f153513e97da877fd2ab6
Masami Ichikawa <masami256@gmail.com>
2025-02-05 09:26:27 +0900

    update
    Signed-off-by: Masami Ichikawa <masami256@gmail.com>

--- setup/build-syzkaller.sh
+++ setup/build-syzkaller.sh
@@ -12,3 +12,6 @@ git checkout f5427d7cf8bff4a8e0647b95351bea3d20e0654e
 make
 
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/syz-cover github.com/google/syzkaller/tools/syz-cover
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/syz-reporter github.com/google/syzkaller/tools/syz-reporter
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/syz-repro github.com/google/syzkaller/tools/syz-repro
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/syz-crush github.com/google/syzkaller/tools/syz-crush
